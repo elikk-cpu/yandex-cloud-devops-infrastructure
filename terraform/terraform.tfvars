b82c0aa99bf7a133c912a9e2bd41634ae37153d8
Andrey <losteli@vk.com>
2026-09-03 11:38:12 +0000

    feat(terraform): initialize yandex cloud provider

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,10 @@
+vpc_name = "devops-practice-vpc"
+
+zone_a = "ru-central1-a"
+zone_b = "ru-central1-b"
+
+public_a_cidr  = "10.10.10.0/24"
+private_a_cidr = "10.10.20.0/24"
+
+public_b_cidr  = "10.10.30.0/24"
+private_b_cidr = "10.10.40.0/24"
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_name" {
+  description = "Name of the main VPC"
+  type        = string
+}
+
+variable "zone_a" {
+  description = "Primary availability zone"
+  type        = string
+}
+
+variable "zone_b" {
+  description = "Secondary availability zone"
+  type        = string
+}
+
+variable "public_a_cidr" {
+  description = "CIDR for public subnet in zone A"
+  type        = string
+}
+
+variable "private_a_cidr" {
+  description = "CIDR for private subnet in zone A"
+  type        = string
+}
+
+variable "public_b_cidr" {
+  description = "CIDR for public subnet in zone B"
+  type        = string
+}
+
+variable "private_b_cidr" {
+  description = "CIDR for private subnet in zone B"
+  type        = string
+}
+
